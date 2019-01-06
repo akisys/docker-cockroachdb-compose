af18d3c7ab9c914095de164b580a1d31f3eb9994
Alexander Kuemmel <akisys@alexanderkuemmel.com>
2019-01-06 19:27:25 +0100

    ADD: COCKROACH_INSECURE option

--- files/roach-certs.sh
+++ files/roach-certs.sh
@@ -3,49 +3,53 @@
 set -ueo pipefail
 
 ROACHBIN="/cockroach/cockroach.sh"
-CERTS_CA_PUB_DIR="${CERTS_CA_PUB_DIR:-$COCKROACH_CERTS_DIR}"
-CERTS_CA_PRIV_DIR="${CERTS_CA_PRIV_DIR:-$COCKROACH_CERTS_DIR}"
-CERTS_NODE_DIR="${CERTS_NODE_DIR:-$COCKROACH_CERTS_DIR}"
-CERTS_CLIENT_DIR="${CERTS_CLIENT_DIR:-$COCKROACH_CERTS_DIR}"
+COCKROACH_INSECURE="${COCKROACH_INSECURE:-"TRUE"}"
 
-mkdir -p "$CERTS_CA_PUB_DIR"
-mkdir -p "$CERTS_CA_PRIV_DIR"
-mkdir -p "$CERTS_NODE_DIR"
-mkdir -p "$CERTS_CLIENT_DIR"
+if [ "$COCKROACH_INSECURE" == "FALSE" ]; then
+  CERTS_CA_PUB_DIR="${CERTS_CA_PUB_DIR:-$COCKROACH_CERTS_DIR}"
+  CERTS_CA_PRIV_DIR="${CERTS_CA_PRIV_DIR:-$COCKROACH_CERTS_DIR}"
+  CERTS_NODE_DIR="${CERTS_NODE_DIR:-$COCKROACH_CERTS_DIR}"
+  CERTS_CLIENT_DIR="${CERTS_CLIENT_DIR:-$COCKROACH_CERTS_DIR}"
 
-if [ ! -f "$CERTS_CA_PRIV_DIR/ca.key" ]; then
-  $ROACHBIN cert create-ca \
-    --certs-dir="$CERTS_CA_PUB_DIR" \
-    --ca-key="$CERTS_CA_PRIV_DIR/ca.key"
+  mkdir -p "$CERTS_CA_PUB_DIR"
+  mkdir -p "$CERTS_CA_PRIV_DIR"
+  mkdir -p "$CERTS_NODE_DIR"
+  mkdir -p "$CERTS_CLIENT_DIR"
 
-  cp -f "$CERTS_CA_PUB_DIR/ca.crt" "$CERTS_NODE_DIR/ca.crt"
-  cp -f "$CERTS_CA_PUB_DIR/ca.crt" "$CERTS_CLIENT_DIR/ca.crt"
-fi
+  if [ ! -f "$CERTS_CA_PRIV_DIR/ca.key" ]; then
+    $ROACHBIN cert create-ca \
+      --certs-dir="$CERTS_CA_PUB_DIR" \
+      --ca-key="$CERTS_CA_PRIV_DIR/ca.key"
 
-CUR_CERTS_NODES="localhost;$CERTS_NODES"
-$ROACHBIN cert create-node $(echo "$CUR_CERTS_NODES" | tr ";" " ") \
-  --certs-dir="$CERTS_NODE_DIR" \
-  --ca-key="$CERTS_CA_PRIV_DIR/ca.key" \
-  --overwrite
+    cp -f "$CERTS_CA_PUB_DIR/ca.crt" "$CERTS_NODE_DIR/ca.crt"
+    cp -f "$CERTS_CA_PUB_DIR/ca.crt" "$CERTS_CLIENT_DIR/ca.crt"
+  fi
 
-for _client in $(echo "$CERTS_CLIENTS" | tr ";" "\n");
-do
-  $ROACHBIN cert create-client "$_client" \
-    --certs-dir="$CERTS_CLIENT_DIR" \
+  CUR_CERTS_NODES="localhost;$CERTS_NODES"
+  $ROACHBIN cert create-node $(echo "$CUR_CERTS_NODES" | tr ";" " ") \
+    --certs-dir="$CERTS_NODE_DIR" \
     --ca-key="$CERTS_CA_PRIV_DIR/ca.key" \
     --overwrite
-done
 
-$ROACHBIN cert list --certs-dir="$CERTS_CA_PUB_DIR"
-$ROACHBIN cert list --certs-dir="$CERTS_NODE_DIR"
-$ROACHBIN cert list --certs-dir="$CERTS_CLIENT_DIR"
+  for _client in $(echo "$CERTS_CLIENTS" | tr ";" "\n");
+  do
+    $ROACHBIN cert create-client "$_client" \
+      --certs-dir="$CERTS_CLIENT_DIR" \
+      --ca-key="$CERTS_CA_PRIV_DIR/ca.key" \
+      --overwrite
+  done
+
+  $ROACHBIN cert list --certs-dir="$CERTS_CA_PUB_DIR"
+  $ROACHBIN cert list --certs-dir="$CERTS_NODE_DIR"
+  $ROACHBIN cert list --certs-dir="$CERTS_CLIENT_DIR"
+
+fi
 
 if [ -z "$(which nc)" ]; then
   apt-get update && apt-get install -y netcat
 fi
-
-for _n in $(echo "$CERTS_NODES" | tr ";" "\n");
-do
+# run this container with minimal overhead
+while true; do
   nc -l -p 8081 -q0
 done
 #EOF
--- files/roach-start.sh
+++ files/roach-start.sh
@@ -4,6 +4,7 @@ set -eu
 
 ROACHBIN="/cockroach/cockroach.sh"
 CERTS_DIR="${COCKROACH_CERTS_DIR:-"/certs/common"}"
+COCKROACH_INSECURE="${COCKROACH_INSECURE:-"TRUE"}"
 
 if [ "${1:-}" == "" ]; then
   /wait-for-it.sh roach-cert-init:8081 -s -t 60 -- $0 start
@@ -20,13 +21,17 @@ fi
 if [ "${1}" == "start" ]; then
   echo "============ START cockroachdb"
   _extra_opts=""
+  if [ "$COCKROACH_INSECURE" == "TRUE" ]; then
+    _extra_opts="--insecure"
+  else
+    _extra_opts="--certs-dir=$CERTS_DIR"
+  fi
   if [ "$PRIMARY_NODE" != "$(hostname)" ]; then
-    _extra_opts="--join=$PRIMARY_NODE"
+    _extra_opts="$_extra_opts --join=$PRIMARY_NODE"
   else
     ($0 wait-init)&
   fi
   exec $ROACHBIN start \
-    --certs-dir="$CERTS_DIR" \
     --advertise-addr="$(hostname)" \
     --listen-addr="0.0.0.0" \
     $_extra_opts
@@ -34,8 +39,12 @@ fi
 
 if [ "${1}" == "init" ]; then
   echo "============ INIT cockroachdb"
-  SQL="$ROACHBIN sql --certs-dir=$CERTS_DIR --host=$PRIMARY_NODE"
-  $SQL -e "CREATE USER IF NOT EXISTS toor WITH PASSWORD 'password';"
+  _extra_opts=""
+  if [ "$COCKROACH_INSECURE" == "FALSE" ]; then
+    _extra_opts="--certs-dir=$CERTS_DIR"
+    SQL="$ROACHBIN sql --host=$PRIMARY_NODE $_extra_opts"
+    $SQL -e "CREATE USER IF NOT EXISTS toor WITH PASSWORD 'password';"
+  fi
 fi
 
-
+#EOF
